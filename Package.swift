90d016bff8dbd1da9b134f8e08c2014663db81d6
Anton Chetverov <678summerdays@gmail.com>
2020-10-13 08:17:04 +0300

    Add MrLotU/SwiftPrometheus

--- Package.swift
+++ Package.swift
@@ -2,25 +2,28 @@
 import PackageDescription
 
 let package = Package(
-    name: "myProject",
-    dependencies: [
-        // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.14.0"),
-        .package(url: "https://github.com/vapor/leaf.git", from: "4.0.0-rc"),
-        .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
-        .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.0-rc")
-    ],
-    targets: [
-        .target(name: "App", dependencies: [
-            .product(name: "Leaf", package: "leaf"),
-            .product(name: "Fluent", package: "fluent"),
-            .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
-            .product(name: "Vapor", package: "vapor"),
-        ]),
-        .target(name: "Run", dependencies: ["App"]),
-        .testTarget(name: "AppTests", dependencies: [
-            .target(name: "App"),
-            .product(name: "XCTVapor", package: "vapor"),
-        ])
-    ]
+  name: "myProject",
+  platforms: [
+    .macOS(.v10_15)
+  ],
+  dependencies: [
+    // 💧 A server-side Swift web framework.
+    .package(url: "https://github.com/vapor/vapor.git", from: "4.14.0"),
+    .package(url: "https://github.com/vapor/leaf.git", from: "4.0.0-rc"),
+    .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
+    .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.0-rc")
+  ],
+  targets: [
+    .target(name: "App", dependencies: [
+      .product(name: "Leaf", package: "leaf"),
+      .product(name: "Fluent", package: "fluent"),
+      .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
+      .product(name: "Vapor", package: "vapor")
+    ]),
+    .target(name: "Run", dependencies: ["App"]),
+    .testTarget(name: "AppTests", dependencies: [
+      .target(name: "App"),
+      .product(name: "XCTVapor", package: "vapor"),
+    ])
+  ]
 )
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -2,23 +2,31 @@ import Leaf
 import Fluent
 import FluentSQLiteDriver
 import Vapor
+import Metrics
 
 public func configure(_ app: Application) throws {
+  app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+  app.views.use(.leaf)
+  app.leaf.cache.isEnabled = app.environment.isRelease
 
-    app.views.use(.leaf)
-    app.leaf.cache.isEnabled = app.environment.isRelease
-    
-    
-    app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
+  app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
 
-    let modules: [Module] = [
-        FrontendModule(),
-        BlogModule(),
-    ]
+  let modules: [Module] = [
+    FrontendModule(),
+    BlogModule(),
+  ]
 
-    for module in modules {
-        try module.configure(app)
-    }
+  for module in modules {
+    try module.configure(app)
+  }
+
+  let myProm = PrometheusClient()
+  MetricsSystem.bootstrap(myProm)
+
+  app.get("metrics") { req -> EventLoopFuture<String> in
+    let promise = req.eventLoop.makePromise(of: String.self)
+    try MetricsSystem.prometheus().collect(into: promise)
+    return promise.futureResult
+  }
 }
